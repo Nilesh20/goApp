13bef05947c55584ff8b4cd85f8830128534101c
Nilesh20 <nil.salunkhe20@gmail.com>
2016-12-08 11:31:29 +0530

    server.go
    server.go file creation

---
+++ server/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    // Standard library packages
+    "net/http"
+
+    // Third party packages
+    "github.com/julienschmidt/httprouter"
+
+    "CloudApp/controllers"
+)
+
+func main() {
+    // Instantiate a new router
+    r := httprouter.New()
+
+    // Get a InstController instance
+    uc := controllers.NewInstController()
+
+    // Get a instance resource
+    r.GET("/dbaas/list", uc.GetDetails)
+
+    r.GET("/dbaas/list/:id", uc.GetDetailsById)
+
+    r.GET("/dbaas/get", uc.GetDB)
+
+    r.GET("/dbaas/pricing", uc.GetPrice)
+
+    // Fire up the server
+    http.ListenAndServe("localhost:3000", r)
+}
